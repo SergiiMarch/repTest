62587f7f860747d862a5b7be9b36b8d41b011f21
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-11 10:57:40 +0300

    add commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEls = document.querySelector('.gallery');
+
+const createImageItem = ({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>    
+    `;
+};
+
+const arrImages = galleryItems.map(createImageItem).join('');
+galleryEls.insertAdjacentHTML('beforeend', arrImages);
+
+galleryEls.addEventListener('click', event => {
+  event.preventDefault();
+  gallery.on('show.Simplelightbox');
+});
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
