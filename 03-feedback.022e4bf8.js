6819567a01fd0f5eea863a1b5f01b206329e9c05
SergiiMarch <111069200+SergiiMarch@users.noreply.github.com>
2023-07-12 19:47:16 +0000

    Deploying to gh-pages from @ SergiiMarch/repTest@67e9de60a094a0f673ee46dc3816400aa683ae6b 🚀

---
+++ 03-feedback.022e4bf8.js
@@ -0,0 +1,2 @@
+!function(){var l=function(l){for(var n=l.split(" "),t=n[0],e=1;e<n.length;e+=1)n[e].length<t.length&&(t=n[e]);return t.length}("hellow my life javaScpt");console.log(l)}();
+//# sourceMappingURL=03-feedback.022e4bf8.js.map
---
+++ 03-feedback.ba86083a.js
@@ -0,0 +1,2 @@
+const l=function(l){let t=l.split(" "),e=t[0];for(let l=1;l<t.length;l+=1)t[l].length<e.length&&(e=t[l]);return e.length}("hellow my life javaScpt");console.log(l);
+//# sourceMappingURL=03-feedback.ba86083a.js.map
