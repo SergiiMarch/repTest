67e9de60a094a0f673ee46dc3816400aa683ae6b
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-12 22:46:51 +0300

    add commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+  throttle(
+    localStorage.setItem('videoplayer-current-time', data.seconds),
+    1000
+  );
+});
+const getTimeVideo = localStorage.getItem('videoplayer-current-time');
+if (getTimeVideo !== null) {
+  player.setCurrentTime(getTimeVideo);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+// ==========================================
+// const STORAGE_KEY = 'feedback-form-state';
+// const inputForm = {
+//   email: '',
+//   message: '',
+// };
 
+// formEl.addEventListener('submit', handleSubmit);
+
+// function formValue(evt) {
+//   evt.target.message.value = inputForm.message;
+//   evt.target.email.value = inputForm.email;
+// }
+
+// function handleSubmit(evt) {
+//   evt.preventDefault();
+//   inputForm[evt.target.name] = evt.target.value;
+// }
+// console.log(inputForm);
+function findShort(s) {
+  let m = s.split(' ');
+  let str = m[0];
+  for (let i = 1; i < m.length; i += 1) {
+    if (m[i].length < str.length) {
+      str = m[i];
+    }
+  }
+  return str.length;
+}
+const shortes = findShort('hellow my life javaScpt');
+console.log(shortes);
