2de71b2d0b032382efba0e5bef0471972e5d54da
Sergii Marchenko <marikmarchelo@gmail.com>
2023-07-15 10:01:54 +0300

    new commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -25,9 +25,33 @@
 //     if (m[i].length < str.length) {
 //       str = m[i];
 //     }
-let a = 5;
+
 //   }
 //   return str.length;
 // }
 // const shortes = findShort('hellow my life javaScpt');
 // console.log(shortes);
+
+// ======================
+
+// const containerEl = document.querySelector('.container');
+
+// const heverHandler = e => {
+//   const { target: button } = e;
+//   button.style.top = `${Math.floor(
+//     Math.random() * (window.innerHeight - button.clientHeight)
+//   )}px`;
+//   button.style.left = `${Math.floor(
+//     Math.random() * (window.innerWidth - button.clientWidth)
+//   )}px`;
+//   console.log(button.clientHeight);
+//   console.log(button.clientWidth);
+// };
+
+// containerEl.addEventListener('mouseover', heverHandler);
+
+function sumMix(x) {
+  const sum = parseInt(...x);
+  console.log(sum);
+}
+console.log(sumMix([9, 3, '7', '3']));
